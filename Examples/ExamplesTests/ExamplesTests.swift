11db950b6e2be5a493cbfcfca5251b18fd6f4832
David Feinzimer <dfeinzimer@esri.com>
2023-08-21 16:54:20 -0700

    Update ExamplesTests.swift

--- Examples/ExamplesTests/ExamplesTests.swift
+++ Examples/ExamplesTests/ExamplesTests.swift
@@ -12,20 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 import XCTest
 @testable import Dejavu
 
 final class ExamplesTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testExample() async throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -44,12 +34,4 @@ final class ExamplesTests: XCTestCase {
         
         await fulfillment(of: [expectation], timeout: 30)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
