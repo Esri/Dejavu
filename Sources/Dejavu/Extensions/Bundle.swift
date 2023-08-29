2cab0bd631e7793a61fa16886e5507ecf411986a
David Feinzimer <dfeinzimer@esri.com>
2023-08-29 10:16:03 -0700

    Remove scoped import

--- Sources/Dejavu/Extensions/Bundle.swift
+++ Sources/Dejavu/Extensions/Bundle.swift
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import Foundation.NSBundle
+import Foundation
 
 extension Bundle {
     /// The identifier for the Dejavu module.
