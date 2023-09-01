587e9f927636f27512ef02819f64e3f5c7c4c41d
David Feinzimer <dfeinzimer@esri.com>
2023-09-01 14:15:26 -0700

    PR feedback

--- Package.swift
+++ Package.swift
@@ -1,11 +1,10 @@
 // swift-tools-version:5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 // Copyright 2023 Esri.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// http://www.apache.org/licenses/LICENSE-2.0
+//   http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
