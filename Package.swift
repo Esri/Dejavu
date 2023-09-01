dde6446995ca7ed2ace54ddeaf068566b0fd3e36
David Feinzimer <dfeinzimer@esri.com>
2023-09-01 16:02:52 -0700

    Remove periods

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 // swift-tools-version:5.7
-// Copyright 2023 Esri.
+// Copyright 2023 Esri
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
